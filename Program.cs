a0744fd4abf3ce745d873b7ee27311b17c503f43
Partha <Partha@LAPTOP-6MONJ5DK>
2021-09-13 20:34:53 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestMSBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
