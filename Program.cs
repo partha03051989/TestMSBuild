b2135d5f59243835486260e804876e820e87d5b1
Parthasarathi Purushothaman <sarathi11par@gmail.com>
2021-09-13 20:56:16 +0530

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace TestMSBuild
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var showResults=Add(9, 10);
+            Console.WriteLine("The Final Results from Addition is:"+showResults);
+
+        }
+        /// <summary>
+        /// adding the and getting two different values as result 
+        /// </summary>
+        /// <param name="val1"></param>
+        /// <param name="val2"></param>
+        /// <returns></returns>
+        public static int Add(int val1,int val2)
+        {
+            if (val1==0)
+            {
+                Console.WriteLine("Enter valid input in order to execute ");
+            }
+            int result = 0;
+            return result = val1 + val2;s
+
         }
     }
 }
